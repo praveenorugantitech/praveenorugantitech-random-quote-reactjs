38c73deb57969a21bad7d12cfab3117113eda541
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-02-02 18:51:45 +0530

    updated

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,50 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Quotes from './Quotes';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Quotes from "./Quotes";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
 const QuoteBox = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const quoteButton = {
+    display: "flex",
+    justifyContent: "center",
+    bottom: "-5em",
+    position: "relative",
+  };
 
-    const [quote, setQuote] = useState('');
-    const [author, setAuthor] = useState('');
-    const quoteButton = {
-        display: "flex",
-        justifyContent: "center",
-        bottom: "-5em",
-        position: "relative"
-    }
+  useEffect(() => {
+    getQuote();
+  }, []);
 
-    useEffect(() => {
-        getQuote();
-    }, [])
-
-    const getQuote = () => {
-        let url = 'https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-test/master/QuotesData/quotes.json'
-        axios.get(url)
-            .then(res => {
-                let data = res.data.quotes;
-                let quoteNum = Math.floor(Math.random() * data.length);
-                let randomQuote = data[quoteNum];
-                setQuote(randomQuote['quote']);
-                setAuthor(randomQuote['author']);
-
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-    return (
-
-        <>
-            <Quotes quote={quote} author={author} />
-            <div style={quoteButton}>
-                <Button variant="contained" onClick={getQuote}>
-                    New Quote
-            </Button>
-            </div>
-        </>
-
-
-    )
-}
+  const getQuote = () => {
+    axios
+      .get("https://60194faefa0b1f0017accd87.mockapi.io/api/v1/quotes")
+      .then((res) => {
+        let data = res.data;
+        let quoteNum = Math.floor(Math.random() * data.length);
+        let randomQuote = data[quoteNum];
+        setQuote(randomQuote["quote"]);
+        setAuthor(randomQuote["author"]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <Quotes quote={quote} author={author} />
+      <div style={quoteButton}>
+        <Button variant="contained" onClick={getQuote}>
+          New Quote
+        </Button>
+      </div>
+    </>
+  );
+};
 
 export default QuoteBox;
